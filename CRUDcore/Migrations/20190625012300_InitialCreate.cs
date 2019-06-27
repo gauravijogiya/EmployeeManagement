2486c525e55fb1fd867549dec974935a3969dd2a
Gauravi <gauravi.jogiya@gmail.com>
2019-06-27 13:35:21 +1000

    InitialCommit

---
+++ CRUDcore/Migrations/20190625012300_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDcore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Fullname = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Empcode = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    position = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    OfficeLocation = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ CRUDcore/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace CRUDcore.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeID { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        [Column(TypeName = "nvarchar(250)")]
+        [DisplayName("Full Name")]
+        public string Fullname { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(10)")]
+        [DisplayName("Emp Code")]
+        public string Empcode { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string position { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
